546cc135523a7cc24ecf86b1945aa748e4debd44
Jiri Peinlich <jiri.peinlich@gmail.com>
2014-07-08 07:55:44 +0200

    adding json path

--- src/test/java/com/barcap/tutorial/ApplicationTests.java
+++ src/test/java/com/barcap/tutorial/ApplicationTests.java
@@ -2,17 +2,19 @@ package com.barcap.tutorial;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
 public class ApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+
+    @Test
+    public void contextLoads() {
+
+    }
 
 }
---
+++ src/test/java/com/barcap/tutorial/controllers/AccountControllerTest.java
@@ -0,0 +1,36 @@
+package com.barcap.tutorial.controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+public class AccountControllerTest {
+
+    @InjectMocks
+    AccountController controller;
+    MockMvc mockMvc;
+
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+
+    @Test
+    public void testGetAccount() throws Exception {
+
+        mockMvc.perform(get("/accounts/1"))
+                .andDo(print())
+                .andExpect(jsonPath("$.id",is(1)));
+    }
+}
